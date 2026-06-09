27f75d68bd3f1d6c8127edb220af211e13b73f45
lorenzobws-sack <lorenzo.bws@aluno.ifsc.edu.br>
2026-06-08 21:31:18 -0300

    versao1.1

--- js/script.js
+++ js/script.js
@@ -1,21 +1,57 @@
 document.addEventListener('DOMContentLoaded', () => {
-    
+
     const config = {
         xokleng: 'xokleng.html',
         guarani: 'guarani.html',
         kaingang: 'kaingang.html'
     };
 
-    Object.keys(config).forEach(classe => {
-        const elementos = document.querySelectorAll(`.${classe}`);
+    document.querySelectorAll("path").forEach(el => {
+        el.style.cursor = 'pointer';
 
-        elementos.forEach(el => {
-            el.style.cursor = 'pointer';
+        el.addEventListener('click', (e) => {
+            e.stopPropagation();
 
-            el.addEventListener('click', () => {
-                window.open(config[classe], '_blank');
-            });
+            if (el.closest("g.kaingang")) {
+                window.open(config.kaingang, '_blank');
+            } 
+            else if (el.closest("g.xokleng")) {
+                window.open(config.xokleng, '_blank');
+            } 
+            else if (el.closest("g.guarani")) {
+                window.open(config.guarani, '_blank');
+            }
         });
     });
+
+});
+
+const legendinha = document.getElementById("legendinha");
+
+
+document.querySelectorAll("g.kaingang path, g.xokleng path, g.guarani path").forEach(el => {
+
+    el.onmousemove = (e) => {
+        legendinha.style.display = "block";
+        legendinha.style.left = e.pageX + 10 + "px";
+        legendinha.style.top = e.pageY + 10 + "px";
+        legendinha.textContent = el.id.replace("_SC", "").replaceAll("_", " ");
+
+         if (el.closest("g.kaingang")) {
+            legendinha.style.background = "#dc143c";
+        } 
+        else if (el.closest("g.xokleng")) {
+            legendinha.style.background = "#05b0b6";
+        } 
+        else if (el.closest("g.guarani")) {
+            legendinha.style.background = "#0aa851";
+        }
+
+    };
+
+    
+    el.onmouseleave = () => {
+        legendinha.style.display = "none";
+    };
 
 });
